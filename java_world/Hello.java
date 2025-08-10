22e0d00a4b6fc4de211ed5404e34b9bb090b8a27
wylee1728 <wylee1728@gmail.com>
2025-08-10 10:46:26 +0900

    Update Hello.java
    xxx

--- java_world/Hello.java
+++ java_world/Hello.java
@@ -3,11 +3,11 @@ package java_world;
 public class Hello {
 	public static void main(String[] args) {
 		//TTT
-		//System.out.println("yyy");
-		System.out.println("yyy");
+		//System.out.println("xxx");
+		System.out.println("xxx");
 		//이게 뭐야 진짜
-		System.out.println("yyy");
-		System.out.println("yyy");
+		System.out.println("xxx");
+		System.out.println("xxx");
 
 		//이게 뭐야 진짜진짜
 	}
