19b9f17903f4245b462d6f931beb31f17d3b1c7c
wylee1728 <wylee1728@gmail.com>
2025-08-10 10:51:53 +0900

    Update Hello.java
    bbb

--- java_world/Hello.java
+++ java_world/Hello.java
@@ -8,10 +8,10 @@ public class Hello {
 		//System.out.println("xxx");
 		System.out.println("xxx");
 		//이게 뭐야 진짜
-		System.out.println("aaa");
-		System.out.println("aaa");
-		System.out.println("aaa");
-		System.out.println("aaa");
+		System.out.println("bbb");
+		System.out.println("bbb");
+		System.out.println("bbb");
+		System.out.println("bbb");
 
 		//이게 뭐야 진짜진짜
 		//이게 뭐야 진짜진짜
